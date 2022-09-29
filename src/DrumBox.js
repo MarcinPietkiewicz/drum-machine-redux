cb1ecf05848dad63fdc2cafe450d060f2ec60c09
Marcin <marcin.p.dev@gmail.com>
2022-09-29 15:31:58 +0200

    Added drum machine container

---
+++ src/DrumBox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './DrumBox.css';
+
+
+class DrumBox extends React.Component {
+
+
+    render() {
+        return (
+            
+            <div id="drum-machine">
+                <div id="display">Display</div>
+            </div>
+        )
+    }
+
+}
+
+
+
+export default DrumBox;
