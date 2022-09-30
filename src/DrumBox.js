d0d07d6f6a3f4ec2a0c2ec6a2b705931614c63f8
Marcin <marcin.p.dev@gmail.com>
2022-09-30 23:01:59 +0200

    Drumbox module changes

--- src/DrumBox.js
+++ src/DrumBox.js
@@ -1,21 +1,25 @@
-import React from 'react';
-import styles from './DrumBox.css';
-
+import React from "react";
+import styles from "./DrumBox.css";
 
 class DrumBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
+  render() {
+    return (
+      <div id="drum-machine">
+        <div id="display">
 
-    render() {
-        return (
-            
-            <div id="drum-machine">
-                <div id="display">Display</div>
-            </div>
-        )
-    }
+            <div className="drum-pad" id=""></div>
 
+        
+        
+        </div>
+      </div>
+    );
+  }
 }
-
-
 
 export default DrumBox;
