5b11af97de4c3ec6f7be3377db807219fa868704
Marcin Pietkiewicz <marcin.p.dev@gmail.com>
2022-10-06 16:45:30 +0200

    Added playing sounds function onClick

--- src/DrumPad.js
+++ src/DrumPad.js
@@ -4,14 +4,22 @@ class DrumPad extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.playSound = this.playSound.bind(this)
+    }
+
+    playSound() {
+        const sound = document.getElementById(this.props.keyTrigger);
+        sound.currentTime = 0;
+        sound.play();
+        setTimeout(() => this.activatePad(), 100);
     }
 
     render() {
         return (
-            <div className='drum-pad' id={this.props.clipId} >
+            <div className='drum-pad' id={this.props.clipId} onClick={this.playSound} >
                 <audio className='clip' id={this.props.keyTrigger} src={this.props.clip} />
                 {this.props.keyTrigger}
-            </div>
+            </div >
         )
     }
 }
