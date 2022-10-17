42324bf474cbe1b64dbb68677a7c203ecd922c1f
Marcin <marcin.p.dev@gmail.com>
2022-10-17 14:30:34 +0200

    Cleaning up project

--- src/App.js
+++ src/App.js
@@ -5,22 +5,20 @@ import DrumBox from "./DrumBox";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { display: "starting..." };
+    this.state = { display: "click any button..." };
     this.updateDisplay = this.updateDisplay.bind(this);
   }
 
   updateDisplay = (clipId) => {
-    this.setState({display: clipId})
-    console.log(clipId);
-    console.log('działa');
-  }
+    this.setState({ display: clipId });
+  };
 
   render() {
     return (
       <div id="center-container">
         <div className="App" id="drum-machine">
           <div id="header">The Drum Machine</div>
-          <DrumBox upDisplay={this.updateDisplay}/>
+          <DrumBox upDisplay={this.updateDisplay} />
           <div id="display">{this.state.display}</div>
         </div>
       </div>
