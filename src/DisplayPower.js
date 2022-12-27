fa1477d3b84d42e5a601e0e107c090d2d1f51b13
Marcin <marcin.p.dev@gmail.com>
2022-12-27 23:32:13 +0100

    Added styling for elements when power button toggle

--- src/DisplayPower.js
+++ src/DisplayPower.js
@@ -10,14 +10,18 @@ class DisplayPower extends React.Component {
   animateLights() {
     let lights = document.getElementById("drum-machine");
     let button = document.getElementById("power-button");
+    let buttonIcon = document.getElementById("power-icon");
+    let title = document.getElementById("header");
     if (button.classList.contains("power-on")) {
       lights.className = "glow";
       button.className = "power-off";
-      button.innerHTML = "PWR<br />OFF";
+      buttonIcon.className = '';
+      title.className='';
     } else {
       lights.className = "glow anim-start";
       button.className = "power-on";
-      button.innerHTML = "PWR<br />ON";
+      buttonIcon.className = 'power-icon-on';
+      title.className = 'active-logo';
     }
   }
 
@@ -26,7 +30,7 @@ class DisplayPower extends React.Component {
       <React.Fragment>
         <div id="display-power"></div>
         <button id="power-button" className="power-off" onClick={this.animateLights}>
-          PWR<br />OFF
+          <img id="power-icon" src="assets/pwr.svg" alt="power button"/>
         </button>
       </React.Fragment>
     );
