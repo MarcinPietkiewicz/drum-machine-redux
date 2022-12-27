039e8747e449b5a8c6893bb75fa42448f037d1af
Marcin <marcin.p.dev@gmail.com>
2022-12-27 23:11:10 +0100

    power button start lights and animation

--- src/DisplayPower.js
+++ src/DisplayPower.js
@@ -1,11 +1,35 @@
 import React from "react";
 
 class DisplayPower extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.animateLights = this.animateLights.bind(this);
+  }
+
+  animateLights() {
+    let lights = document.getElementById("drum-machine");
+    let button = document.getElementById("power-button");
+    if (button.classList.contains("power-on")) {
+        console.log('containes power-on')
+      lights.className = "glow";
+      button.className = "power-off";
+      button.innerHTML = "PWR<br>OFF";
+    } else {
+        console.log('else');
+      lights.className = "glow anim-start";
+      button.className = "power-on";
+      button.innerHTML = "PWR<br>ON";
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <div id="display-power"></div>
-        <div id="power-button">POWER</div>
+        <button id="power-button" className="power-off" onClick={this.animateLights}>
+          PWR<br></br>OFF
+        </button>
       </React.Fragment>
     );
   }
