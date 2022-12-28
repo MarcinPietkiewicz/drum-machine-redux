268809f7e402b64ad5f689a046df191b9b166a50
Marcin <marcin.p.dev@gmail.com>
2022-12-28 11:07:01 +0100

    Added timer for display

--- src/App.js
+++ src/App.js
@@ -2,18 +2,32 @@ import React from "react";
 import "./App.css";
 import DrumBox from "./DrumBox";
 import DisplayInstrument from "./DisplayInstrument";
-import DisplayPower from './DisplayPower';
+import DisplayPower from "./DisplayPower";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { display: "" };
+    this.timerRef = React.createRef();
     this.updateDisplay = this.updateDisplay.bind(this);
+    console.log(this.timerRef.current);
   }
 
-  updateDisplay = (clipId) => {
-    this.setState({ display: clipId });
+  updateDisplay = (text) => {
+    if (this.timerRef.current == null) {
+      this.setDisplayTimer(text);
+    } else {
+      clearTimeout(this.timerRef.current);
+      this.setDisplayTimer(text);
+    }
   };
+
+  setDisplayTimer(text) {
+    this.setState({ display: text });
+    this.timerRef.current = setTimeout(() => {
+      this.setState({ display: "" });
+    }, 1000);
+  }
 
   render() {
     return (
@@ -21,7 +35,7 @@ class App extends React.Component {
         <div className="glow" id="drum-machine">
           <div id="header">Drum Machine Redux</div>
           <DrumBox upDisplay={this.updateDisplay} />
-          <DisplayInstrument display={this.state.display}/>
+          <DisplayInstrument display={this.state.display} />
           <DisplayPower />
         </div>
       </div>
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./DrumPad.css";
+import "./DrumPad.css";
 
 class DrumPad extends React.Component {
   constructor(props) {
