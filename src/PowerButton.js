acf80a47c4be6bfc93f650d903c104d56b9af96f
Marcin <marcin.p.dev@gmail.com>
2022-12-30 18:27:01 +0100

    PowerButton component changed to functional one

--- src/PowerButton.js
+++ src/PowerButton.js
@@ -1,42 +1,35 @@
-import React from "react";
 import pwrSound from "./assets/button.mp3";
 import pwrIcon from "./assets/pwr.svg";
 
-class PowerButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.powerUpDrumBox = this.powerUpDrumBox.bind(this);
-    this.audio = new Audio(pwrSound);
-  }
+function PowerButton(props) {
+  const audio = new Audio(pwrSound);
 
-  powerUpDrumBox() {
+  function powerUpDrumBox() {
     let lights = document.getElementById("drum-machine");
     let button = document.getElementById("power-button");
     let buttonIcon = document.getElementById("power-icon");
     let title = document.getElementById("header");
-    this.audio.play();
+    audio.play();
     if (button.classList.contains("power-on")) {
       lights.className = "glow";
       button.className = "power-off";
       buttonIcon.className = "";
       title.className = "";
-      this.props.powerSwitch();
+      props.powerSwitch();
     } else {
       lights.className = "glow anim-start";
       button.className = "power-on";
       buttonIcon.className = "power-icon-on";
       title.className = "active-logo";
-      this.props.powerSwitch();
+      props.powerSwitch();
     }
   }
 
-  render() {
-    return (
-      <button id="power-button" className="power-off" onClick={this.powerUpDrumBox}>
-        <img id="power-icon" src={pwrIcon} alt="power button" />
-      </button>
-    );
-  }
+  return (
+    <button id="power-button" className="power-off" onClick={powerUpDrumBox}>
+      <img id="power-icon" src={pwrIcon} alt="power button" />
+    </button>
+  );
 }
 
 export default PowerButton;
